f79e81b55b348d2f4029bf63feeffe6d4c418df5
Daniel <daniel.redzynski7@gmail.com>
2024-02-10 13:14:27 +0100

    auth service fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AuthServcie } from './auth/auth.servcie';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  constructor(private aService: AuthServcie, private responsive: BreakpointObserver) {}
+  constructor(
+    private aService: AuthServcie,
+    private responsive: BreakpointObserver
+  ) {}
+
+  public isLogged$: boolean = false;
 
   ngOnInit() {
     this.aService.autoLogin();
+    this.aService.user.subscribe((user) => {
+      user !== null ? (this.isLogged$ = true) : (this.isLogged$ = false);
+      console.log('change', user)
+    });
 
-    this.responsive.observe(Breakpoints.HandsetLandscape)
-    .subscribe(result => {
-
-      if (result.matches) {
-        console.log("screens matches HandsetLandscape");
-      }
-
-});
+    this.responsive
+      .observe(Breakpoints.HandsetLandscape)
+      .subscribe((result) => {
+        if (result.matches) {
+          console.log('screens matches HandsetLandscape');
+        }
+      });
   }
 }
